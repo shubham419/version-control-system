96aa3f824c43795eeedcd6a6327bc9d2629681be
shubham <thoratshubham161@gmail.com>
2023-03-13 09:18:36 +0530

    stage 1

--- Topics/Map/Sum of values/src/Main.kt
+++ Topics/Map/Sum of values/src/Main.kt
@@ -1,3 +1,9 @@
 fun summator(map: Map<Int, Int>): Int {
-    // put your code here
+    var sum: Int = 0
+    for((k, v) in map){
+        if(k % 2 == 0){
+            sum += v
+        }
+    }
+    return sum
 }
