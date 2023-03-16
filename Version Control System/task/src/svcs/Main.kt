9f6491fc3618548ab0a6b79a35730c962ab056a4
shubham <thoratshubham161@gmail.com>
2023-03-16 09:16:38 +0530

    stage 1

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,5 +1,40 @@
 package svcs
 
 fun main(args: Array<String>) {
-
+    when(val command: String? = if (args.isNotEmpty()) args[0] else null){
+        null -> {
+            println("These are SVCS commands:\n" +
+                    "config     Get and set a username.\n" +
+                    "add        Add a file to the index.\n" +
+                    "log        Show commit logs.\n" +
+                    "commit     Save changes.\n" +
+                    "checkout   Restore a file.")
+        }
+        "--help" -> {
+            println("These are SVCS commands:\n" +
+                    "config     Get and set a username.\n" +
+                    "add        Add a file to the index.\n" +
+                    "log        Show commit logs.\n" +
+                    "commit     Save changes.\n" +
+                    "checkout   Restore a file.")
+        }
+        "config" -> {
+            println("Get and set a username.")
+        }
+        "add" ->{
+            println("Add a file to the index.")
+        }
+        "log" -> {
+            println("Show commit logs.")
+        }
+        "commit" -> {
+            println("Save changes.")
+        }
+        "checkout" -> {
+            println("Restore a file.")
+        }
+        else -> {
+            println("'$command' is not a SVCS command.")
+        }
+    }
 }
